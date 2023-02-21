a741e7f1b5f1ee10c370dd8ad338aa9990820eec
Mario Toledo <mariotoledo12@gmail.com>
2023-02-21 02:19:24 -0300

    adding basic HTML and CSS for testing

--- src/tournament-brackets.js
+++ src/tournament-brackets.js
@@ -8,20 +8,94 @@
     - containerElement: the element selector which the brackets are going to be rendered;
     - hasDoubleElimination: if the tournament has the double elimination;
     - showResult: if the brackets should show the result of the match or not;
+    - showControls: if the controls for adding, removing or editing brackets should be visible;
     - data: the data used to render the brackets. It has the following structure:
         - winners: array of arrays, each element represents a round from the winners bracket;
-            - each array inside a round represents a group of players that player that round;
+            - each array inside a round represents a group of players that played that round;
                 - each element inside a group of players is an object with the name of the player, the result and if the player has won or not;
         - losers: array of arrays, each element represents a round from the losers bracket. Has the same structure as the winners;
+
+
+        eg: [{
+            winners: [
+                [
+                    [
+                        {name: 'team 1', result: 3, hasWon: true}, 
+                        {name: 'team 2', result: 1, hasWon: false}
+                    ],
+                    [
+                        {name: 'team 3', result: 2, hasWon: true}, 
+                        {name: 'team 4', result: 0, hasWon: false}
+                    ],
+                    [
+                        {name: 'team 5', result: 2, hasWon: true}, 
+                        {name: 'team 6', result: 1, hasWon: false}
+                    ],
+                    [
+                        {name: 'team 7', result: 4, hasWon: true}, 
+                        {name: 'team 8', result: 2, hasWon: false}
+                    ]
+                ], [
+                    [
+                        {name: 'team 1', result: 4, hasWon: true}, 
+                        {name: 'team 3', result: 3, hasWon: false}
+                    ],
+                    [
+                        {name: 'team 5', result: 2, hasWon: false}, 
+                        {name: 'team 7', result: 1, hasWon: true}
+                    ]
+                ], [
+                    [
+                        {name: 'team 1', result: 3, hasWon: true}, 
+                        {name: 'team 7', result: 2, hasWon: false}
+                    ]
+                ]
+            ],
+            losers: []
+        }]
 */
 
 function TournamentBrackets(options) {
     let defaulltOptions = {
-        containerElement: '.brackets',
+        containerElement: '#tournament-brackets',
         hasDoubleElimination: false,
         showResult: false,
+        showControls: false,
         data: [{
-            winners: [],
+            winners: [
+                [
+                    [
+                        {name: 'team 1', result: 3, hasWon: true}, 
+                        {name: 'team 2', result: 1, hasWon: false}
+                    ],
+                    [
+                        {name: 'team 3', result: 2, hasWon: true}, 
+                        {name: 'team 4', result: 0, hasWon: false}
+                    ],
+                    [
+                        {name: 'team 5', result: 2, hasWon: true}, 
+                        {name: 'team 6', result: 1, hasWon: false}
+                    ],
+                    [
+                        {name: 'team 7', result: 4, hasWon: true}, 
+                        {name: 'team 8', result: 2, hasWon: false}
+                    ]
+                ], [
+                    [
+                        {name: 'team 1', result: 4, hasWon: true}, 
+                        {name: 'team 3', result: 3, hasWon: false}
+                    ],
+                    [
+                        {name: 'team 5', result: 2, hasWon: false}, 
+                        {name: 'team 7', result: 1, hasWon: true}
+                    ]
+                ], [
+                    [
+                        {name: 'team 1', result: 3, hasWon: true}, 
+                        {name: 'team 7', result: 2, hasWon: false}
+                    ]
+                ]
+            ],
             losers: []
         }]
     }
